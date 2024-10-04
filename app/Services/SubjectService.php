5bf73fd89aeea9a6bc01e2d531f48660d6ef6f97
Rodrigo Araujo <rodrigoluz@live.com>
2024-10-04 11:44:00 -0300

    Adicionando regra de negocio ao excluir e salvar objetos

--- app/Services/SubjectService.php
+++ app/Services/SubjectService.php
@@ -3,6 +3,8 @@
 namespace App\Services;
 
 use App\Repositories\SubjectRepository;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Request;
 
 class SubjectService extends BaseService
 {
@@ -11,5 +13,22 @@ class SubjectService extends BaseService
     public function __construct(SubjectRepository $repository)
     {
         $this->repository = $repository;
+    }
+
+    public function save(Request $request): Model
+    {
+        if (strlen(($request->input('descricao'))) > 20) {
+            throw new \Exception('Descrição não pode ser maior que 20 caracteres');
+        }
+        return parent::save($request);
+    }
+
+    public function delete(int|string $id): void
+    {
+        $author = $this->repository->find($id, ['books']);
+        if (count($author->books) > 0) {
+            throw new \Exception('Assunto possui livros relacionados');
+        }
+        parent::delete($id);
     }
 }
